f3da12c70679a2ba17cda4ac3021cac0056ddfd8
primayudantra <prima.yudantra@gmail.com>
2016-07-11 18:39:38 +0700

    fix bootstrap style

--- components/modules/ContactContent.js
+++ components/modules/ContactContent.js
@@ -10,13 +10,15 @@ import { main } from '../styles/style.css'
 class ContactContent extends Component{
   render(){
     return(
-      <div className='container'>
+      <div>
         <MenuList/>
-        <h1>This is Contact</h1>
-        <p className='main'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <Button>Submit</Button>
+        <div className='container'>
+          <h1>This is Contact</h1>
+          <p className='main'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Button>Submit</Button>
+        </div>
       </div>
     )
   }
--- components/modules/MenuListContent.js
+++ components/modules/MenuListContent.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { Navbar, Item, NavbarHeader, NavbarItems } from 'react-bootstrap'
 import { main } from '../styles/style.css'
 
 class MenuList extends Component {
   render(){
     return(
-      <div>
-        <ul role="nav" className="main">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+      <div className="container">
+        <nav className="navbar navbar-default">
+          <div className="container">
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+          </div>
+        </nav>
       </div>
     )
   }
